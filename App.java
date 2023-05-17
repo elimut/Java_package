e3c08c59052c8cd0d219e64693ed8e654ca241eb
elimut <vandingenenannesophie@gmail.com>
2023-05-17 09:50:31 +0200

    maj
    Buffered et Scanner

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ import com.anneSo.Popit;
 import java.util.Scanner;
 // import de la classe Ppopit du package com\anneSo
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -11,7 +12,8 @@ public class App{
     public static String name = "anneso";
     public static String salut = "bonjour";
     public static String firstName = "v";
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception
+    {
         Test ts = new Test("voici mon test");
         System.out.println(ts);
         // affiche thread, espace alloué à var: Test@2f92e0f4
@@ -24,18 +26,25 @@ public class App{
         // Tout cela prend de la mémoire, cela est coûteux
         System.out.println(salut.concat(name).concat(firstName));
         // moins coûteux en mémoire car les variables sont définies dès le départ
-    
-        Scanner scanner = new Scanner(System.in);
-        int reponse = scanner.nextInt();
+        
+        // --------Scanner
+        // Scanner scanner = new Scanner(System.in);
+        // int reponse = scanner.nextInt();
         // attente d'un entier, si l'on a en saisie chaîne de caractères: exceptions => catch, sopit on gère ou non les exceptions. Gestion des exceptions => espace mémoire en plus.
-        System.out.println(reponse);
-        scanner.close();
+        // System.out.println(reponse);
+        // scanner.close();
         // il existe une méthode qui ne traite que des chaînes de caractères, avec optimisation de la mémoire: dans java.io.BufferedReader => si saisie d'un entier convertira en chaîne de caractères.
     
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
+        // ---------BuffereredReader
+        InputStreamReader lecteur = new InputStreamReader(System.in);
+        // create lector
+        BufferedReader entree = new BufferedReader(lecteur);
         // lit ce qu'entre l'user ainsi que d'autres sources: fichier, usb... Il faut importer également import java.io.InputStreamReader, clavier
-        System.out.println(br);
+        String reponse = entree.readLine();
+        System.out.println(reponse);
+        // ajout throws exception voir méthode main
+        entree.close();
+
     }
 }
 /*
